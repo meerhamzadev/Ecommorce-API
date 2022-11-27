f497e19667f82c7303369c8f2804909e54e5d798
meerhamzadev <hamzababar37@gmail.com>
2022-11-27 22:16:23 +0500

    📦 NEW: Add .env support

--- config/config.js
+++ config/config.js
@@ -1,15 +1,17 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    'url': 'postgresql://uc6mjgkx5vra5jvk56sm:7QvXx1OllhjoZZenbUIW@b1has4odgk17fngqihhc-postgresql.services.clever-cloud.com:5432/b1has4odgk17fngqihhc',
-    "dialect": "postgres",
+    'url': process.env.DATABASE_URL,
+    "dialect": process.env.DB,
     logging: false
   },
   test: {
-    url: '127.0.0.1',
-    dialect: "postgres"
+    url: process.env.LOCAL_URL,
+    dialect: process.env.DB
   },
   production: {
     url: process.env.DATABASE_URL,
-    dialect: "postgres"
+    dialect: process.env.DB
   }
 }
